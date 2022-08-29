8bf0ed633bd0e2240c84bda5169bd0bc50e2d725
VV.Koshkin <VV.Koshkin@e-soft.ru>
2022-08-29 19:02:16 +0300

    Довёрстано

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+const checkIfHeaderNeeded = () => {
+    const headerHeight = $('.header').height();
+    const windowScrollPosition = $(window).scrollTop();
+    if (windowScrollPosition > headerHeight) {
+        $('.header').show();
+    } else {
+        $('.header').hide();
+    }
+}
+
 $(() => {
+    // checkIfHeaderNeeded();
     $('#menu-button').click(() => {
         $('.header').toggle();
     });
+    // $(window).on('scroll', checkIfHeaderNeeded);
 });

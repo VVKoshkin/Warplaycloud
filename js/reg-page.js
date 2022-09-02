c5f48315f8a1806b528e2a46781606e2bd22bcd4
VV.Koshkin <VV.Koshkin@e-soft.ru>
2022-09-02 17:06:31 +0300

    Довёрстано всё, добавлено переключение слайдов

--- js/reg-page.js
+++ js/reg-page.js
@@ -4,6 +4,13 @@ $(() => {
         const elem = event.target;
         setPageMode($(elem).data('type'))
     });
+
+    $('input[type="submit"]').click((event) => {
+        event.preventDefault();
+        const elem = event.target;
+        var type = $(elem).parents('form').data('type')
+        formSubmit(type);
+    });
 });
 
 const setPageMode = (type) => {
@@ -11,10 +18,16 @@ const setPageMode = (type) => {
     $(`.reg-option-head[data-type='${type}']`).addClass('active')
     $('.form').addClass('hidden')
     $(`.form[data-type='${type}']`).removeClass('hidden')
-    if(type=='reg') {
+    if (type == 'reg') {
         $('.continue-as').hide()
     } else {
         $('.continue-as').show()
+    }
+}
+
+const formSubmit = (type) => {
+    if (type == 'reg') {
+        window.open('confirmation.html', '_self');
     }
 }
 
--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
-const checkIfHeaderNeeded = () => {
-    const headerHeight = $('.header').height();
-    const windowScrollPosition = $(window).scrollTop();
-    if (windowScrollPosition > headerHeight) {
+$(() => {
+    const sliderChangePicDelaySec = 5;
+    $('#menu-button').click(() => {
         $('.header').show();
-    } else {
+    });
+    $('.header-close').click(() => {
         $('.header').hide();
-    }
+
+    })
+    setInterval(changePicture, 1000 * sliderChangePicDelaySec);
+});
+
+const getNextImage = (curNum, sliderElement) => {
+    const allImageArr = ['1.jfif', '2.jfif', '3.jfif', '4.jfif']
+    let nextNum = parseInt(curNum) + 1;
+    if (nextNum >= allImageArr.length)
+        nextNum = 0;
+    $(sliderElement).data('image-num', nextNum);
+    return `../img/slider/${allImageArr[nextNum]}`
 }
 
-$(() => {
-    // checkIfHeaderNeeded();
-    $('#menu-button').click(() => {
-        $('.header').toggle();
-    });
-    // $(window).on('scroll', checkIfHeaderNeeded);
-});
+const changePicture = () => {
+    const sliderElem = ('.slider');
+    const curNum = $(sliderElem).data('image-num')
+    const nextImageLink = getNextImage(curNum, sliderElem)
+    $('.slider-gradient').css('background-color', 'rgba(0,197,255, 0.7)')
+    setTimeout(() => {
+        $(sliderElem).css('background-image', `url('${nextImageLink}')`)
+        $('.slider-gradient').css('background-color', 'rgba(0,197,255, 0)')
+
+    },500);
+}
+
+const log = (elem) => {
+    console.log(elem)
+}
